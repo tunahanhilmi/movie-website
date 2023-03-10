ac30ca3c2b1ed5e44d9ad1a7fff36aaace381c0b
Tunahan Hilmi <90565601+tunahanhilmi@users.noreply.github.com>
2023-03-11 02:41:52 +0300

    movie-website(v1.0)

---
+++ linkler.js
@@ -0,0 +1,9 @@
+
+
+#vidmoly.onclick=function(){
+    var btn=document.getElementById("iframe");
+        vidmoly.onclick=function(){
+           document.getElementById("iframe").style.display;
+           display:none;
+        }
+    }
